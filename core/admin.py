4c09193e93c879caa711685dced5c6a326abbd42
Richard Smarsi <ricsmarsi@gmail.com>
2022-10-16 16:28:15 -0300

    Adicionado models antigo para backup

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(Partida)
+class PartidaAdmin(admin.ModelAdmin):
+    list_display = ['categoria_perguntas', 'pontuacao']
