443eabafb65f7ed5891c0b642284cd0419f58f8a
Richard Smarsi <ricsmarsi@gmail.com>
2022-11-28 22:15:24 -0300

    Adicionado o diretorio templates para trocar o texto do painel administrativo. Adicionado dependencias faltantes ao requirements.txt

--- quest/settings.py
+++ quest/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': ['templates'],
-        'APP_DIRS': True,
+        #'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
@@ -79,6 +79,10 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': [
+                'django.template.loaders.filesystem.Loader',
+                'django.template.loaders.app_directories.Loader',
+            ]
         },
     },
 ]
